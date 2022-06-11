dfba662972848d673d0fee7fece4eb3eabf4652e
Pavlo Koval <koval.pavlo03@gmail.com>
2022-06-11 19:15:23 +0300

    Added threads

--- main.cpp
+++ main.cpp
@@ -4,19 +4,25 @@
 #include "Searcher.h"
 
 
+void searchWithThreads(Searcher &searcher, const std::string &toFind) {
+    while (not searcher.getIsFound())
+        searcher.tryToSearch(toFind);
+
+}
+
 int main() {
+    const int amountOfThreads = 9;
     const std::string originPath = "/Users/lamens";
     const std::string toFind = "README2.md";
 
+    std::thread threads[amountOfThreads];
     Searcher searcher(originPath);
 
-//    std::thread threadFinder(DeepSearch, std::ref(searcher), std::ref(originPath), std::ref(toFind));
-//    threadFinder.join();
+    for (auto & thread : threads)
+        thread = std::thread(searchWithThreads, std::ref(searcher), std::ref(toFind));
 
-    while (not searcher.getIsFound())
-    {
-        searcher.tryToSearch(toFind);
-    }
+    for (auto & thread : threads)
+        thread.join();
 
     std::cout << searcher.getResult() << std::endl;
     return 0;
